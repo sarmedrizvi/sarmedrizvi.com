6b2baceed9fb65950a3da8551ebc79cf220bbe0e
Tayyabaali02 <tayyaba@impleko.ai>
2025-03-08 03:12:20 +0500

    meta tags added

--- components/sections/Projects2.tsx
+++ components/sections/Projects2.tsx
@@ -269,14 +269,14 @@ export default function Projects2() {
 									</Swiper>
 									<div className="position-absolute bottom-0 end-0 gap-2 pb-7 pe-5 d-none d-md-flex">
 										<div className=" swiper-button-prev end-0 position-relative _swiper-button">
-											<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
+											{/* <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
 												<path d="M7.82843 10.9999H20V12.9999H7.82843L13.1924 18.3638L11.7782 19.778L4 11.9999L11.7782 4.22168L13.1924 5.63589L7.82843 10.9999Z" fill="#ffffff" />
-											</svg>
+											</svg> */}
 										</div>
 										<div className=" swiper-button-next end-0 shadow position-relative _swiper-button">
-											<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
+											{/* <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
 												<path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" fill="#ffffff" />
-											</svg>
+											</svg> */}
 										</div>
 									</div>
 								</div>
