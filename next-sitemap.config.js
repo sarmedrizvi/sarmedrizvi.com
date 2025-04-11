894e4d06e4594da63f71dabb5b6427787242a530
Sarmed <sarmedrizvi@gmail.com>
2025-04-12 03:21:12 +0500

    sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -9,25 +9,26 @@ module.exports = {
   exclude: ["/admin/**", "/404", "/500"],
 
   // Provide paths manually since App Router doesn't expose them automatically
-  additionalPaths: async (config) => {
-    const response = await fetch(`https://sarmedrizvi.com/api/blog-post`, {
-      next: { tags: ["blog-posts"] }, // Optional: for Next.js caching
-    });
+  //   additionalPaths: async (config) => {
+  //     const response = await fetch(`https://sarmedrizvi.com/api/blog-post`, {
+  //       next: { tags: ["blog-posts"] }, // Optional: for Next.js caching
+  //     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! status: ${response.status}`);
+  //     }
 
-    const result = await response.json();
+  //     const result = await response.json();
 
-    if (!result.success || !result.data) {
-      throw new Error(result.message || "Failed to fetch blog posts");
-    }
+  //     if (!result.success || !result.data) {
+  //       throw new Error(result.message || "Failed to fetch blog posts");
+  //     }
 
-    const posts = result.data;
-    const blogs = posts.map((item) => item.id);
-    let paths = ["/", "/blog", ...blogs];
-    paths = [...new Set(paths)];
-    return Promise.all(paths.map((path) => config.transform(config, path)));
-  },
+  //     const posts = result.data;
+  //     const blogs = posts.map((item) => item.id);
+  //     let paths = ["/", "/blog", ...blogs];
+  //     paths = [...new Set(paths)];
+  //     console.log({ paths });
+  //     return Promise.all(paths.map((path) => config.transform(config, path)));
+  //   },
 };
