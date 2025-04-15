ff25e28d8aaa9d15f7b7e76a5ae2cba327aac38d
bkd1-blip <bkd1@alfredus.co>
2025-04-15 21:23:15 +0500

    Merged code after fixes

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { getAllPosts } from "@/util/getAllBlogs";
-import BlogList from "@/components/sections/Blog";
+import BlogList from "@/components/sections/BlogPage";
 
 const BlogsPage = async () => {
   const posts = getAllPosts();
