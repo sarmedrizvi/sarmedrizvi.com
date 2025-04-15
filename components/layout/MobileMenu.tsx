2fdc3d18357639440319be4807590c0eff524c9a
bkd1-blip <bkd1@alfredus.co>
2025-04-15 22:48:06 +0500

    MobileMenu made responsive

--- components/layout/MobileMenu.tsx
+++ components/layout/MobileMenu.tsx
@@ -5,7 +5,10 @@ import { navLinks } from '@/data/header-data';
 
 export default function MobileMenu({ isMobileMenu, handleMobileMenu }: any) {
 	return (
-		<div className={`mobile-header-active mobile-header-wrapper-style perfect-scrollbar button-bg-2 ${isMobileMenu ? 'sidebar-visible' : ''}`}>
+		<div
+			className={`mobile-header-active mobile-header-wrapper-style perfect-scrollbar button-bg-2 ${isMobileMenu ? 'sidebar-visible' : ''
+				}`}
+		>
 			<div className="mobile-header-wrapper-inner">
 				<div className="mobile-header-logo">
 					<Link className="d-flex main-logo align-items-center d-inline-flex" href="/">
@@ -17,7 +20,8 @@ export default function MobileMenu({ isMobileMenu, handleMobileMenu }: any) {
 						<span className="fs-4 ms-2 text-dark">Sarmed.dev</span>
 					</Link>
 					<div
-						className={`burger-icon burger-icon-white border rounded-3 ${isMobileMenu ? 'burger-close' : ''}`}
+						className={`burger-icon burger-icon-white border rounded-3 ${isMobileMenu ? 'burger-close' : ''
+							}`}
 						onClick={handleMobileMenu}
 					>
 						<span className="burger-icon-top" />
@@ -32,7 +36,11 @@ export default function MobileMenu({ isMobileMenu, handleMobileMenu }: any) {
 								<ul className="mobile-menu font-heading ps-0">
 									{navLinks.map(({ href, label }, index) => (
 										<li key={href} className="nav-item">
-											<Link className={`nav-link ${index === 0 ? 'active' : ''}`} href={href}>
+											<Link
+												className={`nav-link ${index === 0 ? 'active' : ''}`}
+												href={href}
+												onClick={handleMobileMenu}
+											>
 												{label}
 											</Link>
 										</li>
